1788e028724f81b5ca1ecb21aaa345bf809c1e9d
Luke DeLeon <lukedeleon94@gmail.com>
2019-08-10 22:31:31 -0700

    repushed index and SchedulerLogic files, this time w/o exposing API key

--- assets/javascript/schedulerLogic.js
+++ assets/javascript/schedulerLogic.js
@@ -1,4 +1,4 @@
-// variables for storing and retrieving data
+//=======================  global vars
 var newTrain;
 var trainAdd;
 var trainName = "";
@@ -8,41 +8,51 @@ var nextArrival;
 var minutesAway = 0;
 var timeTest;
 var trainTime;
+var now = moment().format();
+var today = moment().format("YYYY-MM-DD");
+var todayDateTime = moment().format("YYYY-MM-DD HH:mm:ss");
+var timeDiff;
+var timeRemainder;
+
+
+//======================= global functions 
+
+// displays what time the next train arrives
+function getNextArrivalTime(trainStartTime) {
+    nextArrival = moment(trainStartTime).add((timeDiff + minutesAway), "minutes").format("HH:mm");
+    // console.log("pls work " + nextArrival);
+};
+
+//grabs remaining minutes left until next train shows up
+function getMinutesAway() {
+    var timestamp = $("#train-Start").val().trim();
+    timestamp = moment(timestamp, "HH:mm").format();
+    timeDiff = moment(now).diff(moment(timestamp), "minutes");
+    timeRemainder = timeDiff % frequency;
+    minutesAway = frequency - timeRemainder;
+    getNextArrivalTime(timestamp);
+};
 
 
 $(document).ready(function () {
 
-    // onclick of submit button
     $(".train-Submit").on("click", function (e) {
-        // prevent the default action of the button 
+        //prevents form from resetting 
         e.preventDefault();
-
-        //used to test if an inputted time is BEFORE the train starttime
-        timeTest = moment($("#train-Start").val().trim(), "HH:mm").add($("#train-Freq").val().trim(), "minutes").fromNow();
-
-        console.log("testing ----- " + moment($("#train-Start").val().trim(), "HH:mm").format('LT'));
-        console.log("testing part 2 ---" + (moment($("#train-Start").val().trim(), "HH:mm").fromNow()).includes("ago"));
-        console.log("testing part 3 ---" + (moment($("#train-Start").val().trim(), "HH:mm").fromNow()));
+        var trainStartTime = moment($("#train-Start").val().trim(), "h:mm A").format('HH:mm');
+        var currentTime = moment(now).format('HH:mm');
 
         trainName = $("#train-Name").val().trim();
         destination = $("#train-Destination").val().trim();
-        trainTime = moment($("#train-Start").val().trim(), "HH:mm").format('LT');
+        trainTime = trainStartTime;
         frequency = $("#train-Freq").val().trim();
 
-
-        // fix this -----v
-
-        if ( (moment($("#train-Start").val().trim(), "HH:mm").add($("#train-Freq").val().trim(), "minutes").format("LT")).includes("ago")) {
-            nextArrival = moment($("#train-Start").val().trim(), "HH:mm").format("LT");
+        if (trainStartTime > currentTime) {
+            nextArrival = trainStartTime;
+            minutesAway = moment($("#train-Start").val().trim(), "h:mm A").fromNow('HH:mm');
         } else {
-            nextArrival = moment($("#train-Start").val().trim(), "HH:mm").add($("#train-Freq").val().trim(), "minutes").format("LT");
+            getMinutesAway();
         }
-        if ((moment($("#train-Start").val().trim(), "HH:mm").add($("#train-Freq").val().trim(), "minutes").format("LT")).includes("ago")) {
-            minutesAway = moment($("#train-Start").val().trim(), "HH:mm").fromNow();
-        } else {
-            minutesAway = moment($("#train-Start").val().trim(), "HH:mm").add($("#train-Freq").val().trim(), "minutes").fromNow();
-        }
-
         console.log(trainName);
         console.log(destination);
         console.log(trainTime);
@@ -56,17 +66,17 @@ $(document).ready(function () {
             nextArrival: nextArrival,
             minutesAway: minutesAway
         };
-
+        //initialize DB
         var database = firebase.database();
         // add data to firebase
         database.ref().push(newTrain);
 
-        console.log(newTrain.name);
-        console.log(newTrain.destination);
-        console.log(newTrain.trainTime);
-        console.log(newTrain.frequency);
-        console.log(newTrain.nextArrival);
-        console.log(newTrain.minutesAway);
+        console.log("train name: " + newTrain.name);
+        console.log("train stop: " + newTrain.destination);
+        console.log("train StartTime: " + newTrain.trainTime);
+        console.log("train intervals: " + newTrain.frequency);
+        console.log("next train to arrive: " + newTrain.nextArrival);
+        console.log("train is this far away: " + newTrain.minutesAway);
 
         // add train to table
         trainAdd = $("<tr><th scope='row'>" + newTrain.name + "</th><td>" + newTrain.destination + "</td><td>" + newTrain.frequency + "</td><td>" + newTrain.nextArrival + "</td><td>" + newTrain.minutesAway + "</td></tr>");
